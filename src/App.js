9f58d580418152dd99d78d324303a35ab7bf3d03
Jeremy Velarde <jeremyvelarde@Jeremys-MacBook-Pro-2.local>
2022-01-20 22:15:23 -0500

    update to income functionality and display budget

--- src/App.js
+++ src/App.js
@@ -9,13 +9,13 @@ import Savings from "./components/Savings";
 import Networth from "./components/Networth";
 
 function App() {
-  const [budget, setBudget] = useState([
+  const [budget, setBudget] = useState(
     {
       needs: 0,
       wants: 0,
       savings: 0,
     },
-  ]);
+  );
 
   // Validate input for salary, then calculate budget based on input
   const validateIncome = (e) => {
@@ -25,26 +25,37 @@ function App() {
 
     // Check if input matches regex (numbers only)
     if (!(value.length === 0) && result === true) {
-      // Perform calculations using provided number
-      setBudget([
-        {
-          needs: value * 0.5,
-          wants: value * 0.3,
-          savings: value * 0.2,
-        },
-      ]);
+      let needs = (value * 0.5).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      let wants = (value * 0.3).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      let savings = (value * 0.2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
-      console.log(budget);
+      // let needsFormatted = needs.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      // Perform calculations using provided number
+      setBudget(
+        {
+          needs: needs,
+          wants: wants,
+          savings: savings,
+        },
+      );
+
     } else {
       // TODO: Print error message above or beneath input field
+      setBudget(
+        {
+          needs: 0,
+          wants: 0,
+          savings: 0,
+        },
+      );
     }
   };
 
   return (
     <div className="container">
       <Header />
-      <Income onBlur={validateIncome} />
-      <Budget />
+      <Income onChange={validateIncome} />
+      <Budget income={budget} />
       <Expenses />
       <Leftover />
       <Savings />
