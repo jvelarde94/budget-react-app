a16758d223b1b2585f825df8ea1d91100aea8a46
jvelarde94 <jvelarde94@gmail.com>
2022-03-30 02:49:30 -0400

    update placement of header on desktop and mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className="header">
       {/* <Typography variant="h3">Super Simple Budget Tool Manager 9000</Typography> */}
-      <h1 className="header-title">
+      {/* <h1 className="header-title">
         Super Simple Budget Manager Tool
         <span className="version-number">
           {" "}
@@ -13,7 +13,20 @@ const Header = () => {
           <img src="vegeta-over-9000.png" className="img" />)
         </span>
       </h1>
-      <span className="small-funny-text">By Jeremy Velarde</span>
+      <span className="small-funny-text">By Jeremy Velarde</span> */}
+
+
+      <div className="header-title">
+        <span>Super Simple</span><br/> 
+        <span>Budget Manager</span><br/> 
+        <span>Tool</span>
+      </div>
+        <span className="version-number">
+          {" "}
+          v9000.1
+          <img src="vegeta-over-9000.png" className="img" />
+        </span>
+        <span className="small-funny-text">By Jeremy Velarde</span>
     </div>
   );
 };
