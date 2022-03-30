85e3bfef4913300cc580425159bc5bdbc5604809
jvelarde94 <jvelarde94@gmail.com>
2022-03-30 03:28:16 -0400

    update modal list spacing

--- src/components/Info.js
+++ src/components/Info.js
@@ -26,15 +26,14 @@ const Info = () => {
                 <div className="main-info">
                     <h5>This tool is based on the 50/30/20 budget rule:</h5>
                     <ul>
-                        <li>
+                        <li className="modal-list">
                             50% of your income: needs. This portion of your budget should cover necessary costs, such as:
                             <ul className="example-expenses">
                                 <li>Rent/Mortgage</li>
                                 <li>Car payments</li>
                             </ul>
                         </li>
-                        <br />
-                        <li>
+                        <li className="modal-list">
                             30% of your income: wants. Wants are the fun stuff; things you don't require, but desire. Examples include:
                             <ul className="example-expenses">
                                 <li>Monthly subscriptions</li>
@@ -42,8 +41,7 @@ const Info = () => {
                                 <li>Travel</li>
                             </ul>
                         </li>
-                        <br />
-                        <li>
+                        <li className="modal-list">
                             20% of your income: savings. This is the amount you are aiming to save, ideally for future spending, at the least. Examples for what to save money for include:
                             <ul className="example-expenses">
                                 <li>Emergency fund</li>
@@ -56,7 +54,7 @@ const Info = () => {
                 <h4 className="modal-section-header">How do I use this?</h4>
                 <hr />
                 <ol>
-                    <li>
+                    <li className="modal-list">
                         Enter your Annual Income&nbsp;
                         <span className="extra-info">
                         (after-tax preferred for more accuracy - you can calculate this using&nbsp;
@@ -65,20 +63,13 @@ const Info = () => {
                         </span>
                         This will calculate your monthly budget according to the 50/30/20 budget rule.
                     </li>
-                    <br />
-                    <li>
+                    <li className="modal-list">
                         Add your monthly expenses and categorize each expense as a "need" or "want." This will update your monthly budget amounts accordingly.
                     </li>
-                    <br />
-                    <li>
+                    <li className="modal-list">
                         Your overall savings is a combination of Savings and any leftover money from "Wants" or "Needs."
                     </li>
-                    <br />
-                    <li>
-                        Use the <span style={{color: "red"}}>Reset</span> button to clear everything and start over.
-                    </li>
-                    <br />
-                    <li>
+                    <li className="modal-list">
                         Do as you please with this information.
                     </li>
                 </ol>
